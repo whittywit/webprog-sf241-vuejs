2b50ae31c809f7c44c6be58086b67b762914a7c8
Daniel James Whitwell <djwhitwell@student.apc.edu.ph>
2026-02-15 19:35:36 +0800

    another color

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -2,6 +2,7 @@
   <h1>Food</h1>
   <food-item/>
   <food-item2/>
+
 </template>
 
 <script></script>
--- firstsfc/src/components/FoodItem2.vue
+++ firstsfc/src/components/FoodItem2.vue
@@ -22,6 +22,6 @@ export default {
     display: inline-block;
     margin: 10px;
     padding: 10px;
-    background-color: lightgreen;
+    background-color: #54F527;
   }
 </style>
