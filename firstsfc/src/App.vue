a4b27f6af6d9065b211c1831ad728547a2a8cfa7
Daniel James Whitwell <djwhitwell@student.apc.edu.ph>
2026-02-15 10:55:23 +0000

    clean project

---
+++ firstsfc/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Food</h1>
+  <food-item/>
+  <food-item/>
+  <food-item/>
+</template>
+
+<script></script>
+
+<style>
+  #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+</style>
