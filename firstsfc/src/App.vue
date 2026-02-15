c27af8b9daff1dbe6b00b38c74c926716e91d212
Daniel James Whitwell <djwhitwell@student.apc.edu.ph>
2026-02-15 19:24:53 +0800

    fooditems

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -7,12 +7,4 @@
 
 <script></script>
 
-<style>
-  #app > div {
-    border: dashed black 1px;
-    display: inline-block;
-    margin: 10px;
-    padding: 10px;
-    background-color: lightgreen;
-  }
-</style>
+<style></style>
