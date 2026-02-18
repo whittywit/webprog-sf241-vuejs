3642ced45c01ad5ecf0125fc992c402e5958f814
Daniel James Whitwell <djwhitwell@student.apc.edu.ph>
2026-02-18 14:39:39 +0800

    Task 04 Create the Supabase client in your vercel hosted site

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,10 +1,21 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
+
+const instruments = ref([])
+
+async function getInstruments() {
+  const { data } = await supabase.from('instruments').select()
+  instruments.value = data
+}
+
+onMounted(() => {
+   getInstruments()
+})
+</script>
+
 <template>
-  <h1>Food</h1>
-  <food-item/>
-  <food-item2/>
-
+  <ul>
+    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+  </ul>
 </template>
-
-<script></script>
-
-<style></style>
